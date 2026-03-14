f8c0bfd111b59c42ca1ca2615d4b6b974b97f992
httpsJadson <dev.jadson27@gmail.com>
2026-03-14 12:04:26 -0300

    feat: add user findAll route

---
+++ src/main/java/com/jadsonspring/course/resources/UserResource.java
@@ -0,0 +1,17 @@
+package com.jadsonspring.course.resources;
+import com.jadsonspring.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping()
+    public ResponseEntity<User> FindAll(){
+        User u = new User(1, "Jadson", "888888888", "jadson@email.com", "123456");
+        return ResponseEntity.ok().body(u);
+    }
+}

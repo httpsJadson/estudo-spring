3b4dea5210f3221c843da71f08e717d712c486fe
httpsJadson <dev.jadson27@gmail.com>
2026-03-14 16:14:39 -0300

    feat: add depencies for H2 database for tests and configurate users route

--- src/main/java/com/jadsonspring/course/entities/User.java
+++ src/main/java/com/jadsonspring/course/entities/User.java
@@ -1,10 +1,15 @@
 package com.jadsonspring.course.entities;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String phone;
